096c8763351eb773acabac80d83a917288838557
gagandeep0810 <gagans0179@gmail.com>
2025-03-03 14:09:28 +0530

    Login Form Heading Changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,8 +25,8 @@ const Login = ({ setIsAuthenticated }) => {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8 bg-white">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-              Sign in to your account
+            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+              LOGIN
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
